8586140b638acf4236c9ce7ce821aa49a917823a
j000 <jarymut+github@gmail.com>
2021-03-01 12:50:54 +0100

    Add dimensions

--- zad1/src/main.cxx
+++ zad1/src/main.cxx
@@ -1,34 +1,39 @@
 #include <array>
 #include <cmath>
 #include <gsl/gsl_randist.h>
+#include <iomanip>
 #include <iostream>
 #include <numeric>
 #include <sys/time.h>
 
-constexpr int N{4096};
+constexpr int N{1 << 12};
+constexpr int DIMS{2};
 
 constexpr double pi()
 {
 	return std::atan(1) * 4;
 }
 
-double f(const double x)
-{
-	return std::sin(x);
-}
-
 int main(int, char**)
 {
 	auto generator = gsl_rng_alloc(gsl_rng_ranlux);
-	struct timeval tv; // Seed generation based on time
-	gettimeofday(&tv, 0);
-	unsigned long seed = tv.tv_sec + tv.tv_usec;
-	gsl_rng_set(generator, seed); // seed
+
+	{ // Seed generation based on time
+		struct timeval tv;
+		gettimeofday(&tv, 0);
+		unsigned long seed = tv.tv_sec + tv.tv_usec;
+		gsl_rng_set(generator, seed);
+	}
+
 	std::array<double, N> sum{};
 
 	for (auto& i : sum) {
-		double x{pi() * gsl_rng_uniform(generator)};
-		i = f(x);
+		auto result{1.};
+		for (int j = 0; j < DIMS; ++j) {
+			auto x = pi() * gsl_rng_uniform(generator);
+			result *= std::sin(x);
+		}
+		i = result;
 	}
 
 	double average{std::accumulate(sum.begin(), sum.end(), 0.) / N};
@@ -42,8 +47,12 @@ int main(int, char**)
 
 	double error{sigma_y * pi() / std::sqrt(N)};
 
-	std::cout << "result: " << average * pi() << std::endl;
-	std::cout << "error: " << error << std::endl;
+	std::cout << std::scientific;
+	std::cout << std::setw(20) << "result - expected = " << std::setw(13)
+			  << average * std::pow(pi(), DIMS) - std::pow(2., DIMS)
+			  << std::endl;
+	std::cout << std::setw(20) << "error = " << std::setw(13) << error
+			  << std::endl;
 
 	gsl_rng_free(generator);
 }
